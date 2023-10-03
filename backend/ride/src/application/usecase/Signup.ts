33360be482da0e9079ce5c1fce4ea9111a71b9ba
Augusto <augustoscher@gmail.com>
2023-10-03 18:20:07 -0300

    feat: add signup usecase

---
+++ backend/ride/src/application/usecase/Signup.ts
@@ -0,0 +1,53 @@
+import AccountDAO from '../../dao/account/AccountDAO'
+import MailerGateway from '../../gateway/MailerGateway'
+import AccountDAODatabase from '../../dao/account/AccountDAODatabase'
+import Account from '../../domain/Account'
+
+export type SignupParams = {
+  name: string
+  email: string
+  cpf: string
+  isPassenger: boolean
+  isDriver: boolean
+  carPlate: string
+}
+
+export default class Signup {
+  mailerGateway: MailerGateway
+
+  // creating a port for more than one adapter to implement, allowing me to vary the behavior
+  constructor(readonly accountDAO: AccountDAO = new AccountDAODatabase()) {
+    this.mailerGateway = new MailerGateway()
+  }
+
+  async execute({
+    email,
+    name,
+    cpf,
+    isPassenger,
+    isDriver,
+    carPlate
+  }: SignupParams) {
+    const existingAccount = await this.accountDAO.getByEmail(email)
+    if (existingAccount) throw new Error('Account already exists')
+
+    const account = Account.create({
+      name,
+      email,
+      cpf,
+      isPassenger,
+      isDriver,
+      carPlate
+    })
+
+    await this.accountDAO.save(account)
+    await this.mailerGateway.send(
+      email,
+      'Verification',
+      `Please verify your code at first login ${account.verificationCode}`
+    )
+    return {
+      accountId: account.accountId
+    }
+  }
+}
